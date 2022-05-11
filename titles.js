f78bedc67da2d2f96e8235c0a1479657e330708c
kpooml <reddituplike@gmail.com>
2022-05-11 21:00:11 +0300

    test

---
+++ titles.js
@@ -0,0 +1,9 @@
+document.getElementById("gerifer").title = "Wither s skull - 2 iris; \nLava bucket - 2 diamond; \n10 TNT - 9 diamonds;";
+document.getElementById("fisherman").title = "Castaway - 2 iris; \n32 Raw cod - 8 iron; \nRaw salmon - 8 iron;";
+document.getElementById("egapseller").title = "Egapple - 3 iris;";
+document.getElementById("bartender").title = "Half beer - 2 iron; \nBeer - 4 iron";
+document.getElementById("witch").title = "Loki's chestplate - 1 iris; \nPotion of strength - 2 diamonds; \nPotion of regeneration - 2 diamonds; \nPotion of night vision - 2 diamonds; \nPotion of slow falling - 2 diamonds; \nPotion of fire resistance - 2 diamonds; \nPotion of swiftness - 2 diamonds;";
+document.getElementById("carrots").title = "8 Gapples - 1 iris; \n64 Golden carrots - 1 iris;";
+document.getElementById("wool").title = "White wool - 4 iron; \nOrange wool - 4 iron; \nMagenta wool - 4 iron; \nLight blue wool - 4 iron; \nYellow wool - 4 iron; \nLime wool - 4 iron; \nPink wool - 4 iron; \nGray wool - 4 iron; \nLight gray wool - 4 iron; \nCyan wool - 4 iron; \nPurple wool - 4 iron; \nBlue wool - 4 iron; \nBrown wool - 4 iron; \nGreen wool - 4 iron; \nRed wool - 4 iron; \nBlack wool - 4 iron;";
+document.getElementById("maps").title = "64 Empty map - 1 iris;";
+document.getElementById("rockets").title = "64 Rockets(duration 1) - 1 iris; \n64 Rockets(duration 2) - 1 iris; \n64 Rockets(duration 3) - 1 iris;";
